981b234aad233963b669251ca9cfecfdbc04fffa
JulietPolokSarkar <julietpoloksarkar@gmail.com>
2023-08-10 01:04:35 +0200

    packages added

---
+++ src/main/java/com/example/randdpersonaldata/RandDpersonalDataApplication.java
@@ -0,0 +1,13 @@
+package com.example.randdpersonaldata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandDpersonalDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RandDpersonalDataApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/randdpersonaldata/RandDpersonalDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.randdpersonaldata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RandDpersonalDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
